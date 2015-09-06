b98b7d423dc71d23e47a7f193a8f8ca8c86a9d96
Amanleen Puri <amanleen@Amanleens-MacBook-Pro.local>
2015-09-06 15:06:57 -0700

    Working with bootstrap

--- public/js/chatroom_client.js
+++ public/js/chatroom_client.js
@@ -55,10 +55,13 @@ $(function(){
 			glatestMessageTimestamp = messageDetails.activitytimestamp;
 			var messageDateTime = new Date(messageDetails.activitytimestamp*1000);
 			var creationTime = formatDateTime(messageDateTime);
-			content = '<table class="chatroom-list-element-table"><tr class="chatroom-list-header"><td class="chatroom-list-header-name">'+messageDetails.username+'</td><td class="chatroom-list-header-date">'+creationTime+'</td></tr><tr class="chatroom-list-body"><td colspan=2>'+messageDetails.textMessage+'</td></tr></table>';
-      		list.push($('<li>', { html: content }));
+			//content = '<table class="chatroom-list-element-table"><tr class="chatroom-list-header"><td class="chatroom-list-header-name">'+messageDetails.username+'</td><td class="chatroom-list-header-date">'+creationTime+'</td></tr><tr class="chatroom-list-body"><td colspan=2>'+messageDetails.textMessage+'</td></tr></table>';
+			//$('#message-list').append('<div class="row chatroom-list-message"><div class="row col-lg-offset-2 col-lg-6 chatroom-list-message-row"><div class="text-left col-lg-6 chatroom-list-header-name"> '+messageDetails.username+' </div><div class="col-lg-6 text-left chatroom-list-header-date pull-right"> '+creationTime+'</div></div><div class="row"><div class="col-lg-6 col-lg-offset-2 text-left chatroom-list-message-row chatroom-list-body">'+messageDetails.textMessage+'</div></div></div>');
+			$('#message-list').append('<div class="row col-md-offset-2 col-md-6 col-lg-offset-2 col-lg-6 chatroom-list-message-row"><div class="row"><span class="text-left chatroom-list-header-name"> '+messageDetails.username+' </span><span class="pull-right chatroom-list-header-date pull-right"> '+creationTime+'</span></div><div class="row"><span class="text-left chatroom-list-body">'+messageDetails.textMessage+'</span></div></div>');
+			//$('#message-list').append('<div class="row"><div class="col-lg-offset-2 col-lg-6 text-left chatroom-list-body chatroom-list-element-table">'+messageDetails.textMessage+'</div></div>');
+			//content = '<div class="row"><div class="col-lg-1"> '+messageDetails.username+' </div> <div class="col-lg-2"/><div class="col-lg-1"> '+creationTime+'</div></div><div class="row">'+messageDetails.textMessage+'</div>'
+      		//list.push($('<div class="row">', { html: content }));
 		}
-		$('.list-unstyled').append(list);
 	}
 
 	function getMessageHistory(){
@@ -72,17 +75,17 @@ $(function(){
   	}
   	getMessageHistory();
 
-	$('.chatmessage-form').on('submit', function(event){
-  		event.preventDefault(); 
-  		var form = $(this);
-  		var messageData = form.serialize();
-  		messageData=messageData+"&userId="+guser.id+"&chatroomId="+gchatroomId;
+	$('#submit-message').click(function(event){
+  		event.preventDefault();
+  		var messageText = encodeURIComponent($('#messageText').val());
+  		var messageData="message="+messageText+"&userId="+guser.id+"&chatroomId="+gchatroomId;
+  		console.log(messageData)
   		// alert(messageData);
 	  	$.ajax({
 	  		type:'POST', url:'/chatroom', data:messageData
 	  	}).done(function(reply){
 	  		appendMessageList(reply);
-	  		form.trigger('reset');
+	  		$('#messageText').val('');
 		});
 	});
 });
